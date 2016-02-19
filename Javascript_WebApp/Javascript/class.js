59122fcec48093257568b2fa845eedf4153e0af0
sittiporn <sittiporn@silkspan.local>
2016-02-19 14:58:53 +0700

    Javascript class

---
+++ Javascript_WebApp/Javascript/class.js
@@ -0,0 +1,61 @@
+﻿
+/////////////////////////////////
+////  class 
+/* 
+var call = new callClass();
+console.log(" call B", call.cClassB(1234))
+
+ // class not return
+    call.cClassD()
+
+
+*/
+///////////////////////////////// 
+
+var callClass = function () {
+    // constructure
+}; 
+
+callClass.prototype.cClassA = function() {
+    return Message.cClassC();
+};
+ 
+callClass.prototype.cClassB= function(obj) {
+    return obj
+};
+
+
+callClass.prototype.cClassC = function() {
+    var data = { data1: "date1", data2: "data2" }
+    return data;
+};
+
+
+callClass.prototype.cClassD = function () {
+    var data = { data1: "date1", data2: "data2" }
+    console.log(data);
+};
+
+
+
+/////////////////////////////////
+//// แบบ ย่อ
+/* 
+    var call2 = new callClass2();
+    console.log(call2.A(), call2.B());
+*/
+/////////////////////////////////
+
+var callClass2 = function () {
+    // constructure
+};
+
+callClass2.prototype = {
+    A: function A() {
+        return "class A";
+    },
+    B: function B() {
+        return "class B";
+    } 
+};
+  
--- Javascript_WebApp/WebForm/Default.aspx
+++ Javascript_WebApp/WebForm/Default.aspx
@@ -12,7 +12,7 @@
     
     </div>
     </form>
-    <script src="../Javascript/staticClass.js" type="text/javascript"></script>
+    <%--<script src="../Javascript/staticClass.js" type="text/javascript"></script>
     <script>
         // call class A : 
         console.log(callClass.cClassA());
@@ -21,6 +21,18 @@
         console.log(callClass.cClassB(_object));
         // call class C : 
         console.log(callClass.cClassC());
+    </script>--%>
+
+    <script src="../Javascript/class.js" type="text/javascript"></script>
+    <script> 
+        var call = new callClass(); 
+        console.log(" call B", call.cClassB(1234)) 
+        // class D Not Return Type
+        call.cClassD()
+         
+        var call2 = new callClass2();
+        console.log(call2.A(), call2.B()); 
     </script>
+     
 </body>
 </html>
